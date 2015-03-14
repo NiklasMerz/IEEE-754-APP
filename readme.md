2046922d23c71de01b46b009b8fbfb628409b1db
Niklas Merz <NiklasMerz@gmx.net>
2015-03-14 16:35:56 +0100

    Erste Binärzahl wird angezeigt

---
+++ readme.md
@@ -0,0 +1,4 @@
+# IEEE-754-APP
+Schulprojekt
+
+App zum Anzeigen von Gleitkommazahlen nach IEE 754
--- src/de/merz/ieee_754/MainActivity.java
+++ src/de/merz/ieee_754/MainActivity.java
@@ -6,20 +6,40 @@ import android.R.string;
 import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
+import android.text.Editable;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 @SuppressWarnings("unused")
 public class MainActivity extends Activity{
-
+	
+	float number;
+	Integer bits;
+	String result; 
+		
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
+	
+	public void Calculate(View view){
+		
+		EditText nField = (EditText)findViewById(R.id.editText1);
+		TextView text=(TextView)findViewById(R.id.textView1);
+		
+		number = Float.valueOf(nField.getText().toString());
+		bits = Float.floatToRawIntBits(number);
+		result = Integer.toBinaryString(bits);
+		
+		text.setText(result);
+		
+	}
+	
+	
+	
+	
 
 	}
