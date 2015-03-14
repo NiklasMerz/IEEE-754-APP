70845da839b7649b77fd2261f7c96e9bbad9ef68
Niklas Merz <NiklasMerz@gmx.net>
2015-03-14 19:35:06 +0100

    Konvertirerung und Darstellung vorhanden

--- gen/de/merz/ieee_754/R.java
+++ gen/de/merz/ieee_754/R.java
@@ -25,59 +25,48 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int button1=0x7f070002;
+        public static final int button1=0x7f070001;
         public static final int editText1=0x7f070000;
-        public static final int textView1=0x7f070001;
+        public static final int textView1=0x7f070003;
+        public static final int textView2=0x7f070005;
+        public static final int textView3=0x7f070004;
+        public static final int textView4=0x7f070002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class string {
         public static final int Calculate=0x7f050003;
+        public static final int Ch=0x7f050008;
+        public static final int Check=0x7f05000a;
         public static final int Coded=0x7f050005;
         public static final int DefaultValue=0x7f050006;
         public static final int Float=0x7f050004;
+        public static final int Man=0x7f050009;
+        public static final int Pre=0x7f050007;
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050001;
     }
     public static final class style {
         /** 
-        Base application theme, dependent on API level. This theme is replaced
-        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
+Base application theme for API 14+. This theme completely replaces
+AppBaseTheme from BOTH res/values/styles.xml and
+res/values-v11/styles.xml on API 14+ devices.
     
+ API 14 theme customizations can go here. 
 
-            Theme customizations available in newer API levels can go in
-            res/values-vXX/styles.xml, while customizations related to
-            backward-compatibility can go here.
-        
-
-        Base application theme, dependent on API level. This theme is replaced
-        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
+Base application theme for API 11+. This theme completely replaces
+AppBaseTheme from res/values/styles.xml on API 11+ devices.
     
+ API 11 theme customizations can go here. 
 
-            Theme customizations available in newer API levels can go in
-            res/values-vXX/styles.xml, while customizations related to
-            backward-compatibility can go here.
-        
-
-        Base application theme, dependent on API level. This theme is replaced
-        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
+Base application theme for API 14+. This theme completely replaces
+AppBaseTheme from BOTH res/values/styles.xml and
+res/values-v11/styles.xml on API 14+ devices.
     
-
-            Theme customizations available in newer API levels can go in
-            res/values-vXX/styles.xml, while customizations related to
-            backward-compatibility can go here.
-        
+ API 14 theme customizations can go here. 
          */
         public static final int AppBaseTheme=0x7f060000;
-        /**  Application theme. 
- All customizations that are NOT specific to a particular API-level can go here. 
- Application theme. 
- All customizations that are NOT specific to a particular API-level can go here. 
- Application theme. 
- All customizations that are NOT specific to a particular API-level can go here. 
-         */
-        public static final int AppTheme=0x7f060001;
     }
 }
--- src/de/merz/ieee_754/MainActivity.java
+++ src/de/merz/ieee_754/MainActivity.java
@@ -13,33 +13,85 @@ import android.widget.TextView;
 
 @SuppressWarnings("unused")
 public class MainActivity extends Activity{
-	
+
+	int i;
 	float number;
 	Integer bits;
-	String result; 
-		
-	
+	String result, check;
+	String vorzeichen, charakteristik, mantisse;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
-	
+
 	public void Calculate(View view){
-		
+
 		EditText nField = (EditText)findViewById(R.id.editText1);
-		TextView text=(TextView)findViewById(R.id.textView1);
-		
+		TextView text1=(TextView)findViewById(R.id.textView1);
+		TextView text2=(TextView)findViewById(R.id.textView2);
+		TextView text3=(TextView)findViewById(R.id.textView3);
+		TextView text4=(TextView)findViewById(R.id.textView4);
+
+		Delete();
+
 		number = Float.valueOf(nField.getText().toString());
 		bits = Float.floatToRawIntBits(number);
-		result = Integer.toBinaryString(bits);
+		result = "0" + Integer.toBinaryString(bits); //Negative Werte
+
+		char[] chars = result.toCharArray();
+
+		vorzeichen = Character.toString(chars[0]);
+
+		for(i = 1; i <= 8; i++){
+			charakteristik = charakteristik + Character.toString(chars[i]);			
+		}
+
+		for(i = 9; i <= 31; i++){
+			mantisse = mantisse + Character.toString(chars[i]);			
+		}
+
+		text1.setText(vorzeichen);
+		text1.setTextColor(Color.RED);
+
+		text2.setText(charakteristik);
+		text2.setTextColor(Color.YELLOW);
+
+		text3.setText(mantisse);
+		text3.setTextColor(Color.GREEN);
 		
-		text.setText(result);
-		
-	}
-	
-	
-	
-	
+		check = vorzeichen + charakteristik + mantisse;
+
+		if(result.equals(check)){
+			text4.setText("true");
+		}else{
+			text4.setText(result + "/" + System.getProperty ("line.separator") + check + "//" + chars.length);
+		}
 
 	}
+
+	private void Delete(){
+
+		EditText nField = (EditText)findViewById(R.id.editText1);
+		TextView text1=(TextView)findViewById(R.id.textView1);
+		TextView text2=(TextView)findViewById(R.id.textView2);
+		TextView text3=(TextView)findViewById(R.id.textView3);
+		TextView text4=(TextView)findViewById(R.id.textView4);
+
+		text1.setText(vorzeichen);
+		text2.setText(charakteristik);
+		text3.setText(mantisse);
+		text4.setText(result);
+
+		number = 0;
+		bits = 0;
+		result = "";
+		vorzeichen = "";
+		charakteristik = "";
+		mantisse = "";
+	}
+
+
+
+}
