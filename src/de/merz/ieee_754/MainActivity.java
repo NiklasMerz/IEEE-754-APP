1c068e62a873135bc2288940327f0f96142e5c56
Niklas Merz <NiklasMerz@gmx.net>
2015-03-14 15:53:46 +0100

    Initialer commit

---
+++ src/de/merz/ieee_754/MainActivity.java
@@ -0,0 +1,25 @@
+package de.merz.ieee_754;
+
+
+import de.merz.ieee_754.R;
+import android.R.string;
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+@SuppressWarnings("unused")
+public class MainActivity extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	}
